6c328a8f79c35223cefb9b4488fea07c6d79f3c8
luxuan <myfather103@163.com>
2019-06-22 09:38:40 +0800

    引入检测器，检测人脸。

--- app/src/main/jni/DectectionBasedTracker_jni.cpp
+++ app/src/main/jni/DectectionBasedTracker_jni.cpp
@@ -9,6 +9,8 @@
 #include <string>
 #include <vector>
 
+#include <android/log.h>
+
 #define LOG_TAG "FaceDetection/DetectionBasedTracker"
 #define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__))
 
@@ -17,5 +19,33 @@ using namespace cv;
 
 inline void vector_Rect_to_Mat(vector<Rect>& v_ret, Mat& mat)
 {
-    mat=Mat(v_rect, true);
+    mat=Mat(v_ret, true);
 }
+
+class CascadeDetectorAdapter: public DetectionBasedTracker::IDetector
+{
+public:
+    CascadeDetectorAdapter(cv::Ptr<cv::CascadeClassifier> detector):
+        IDetector(),
+        Detector(detector)
+    {
+        LOGD("CascadeDetectorAdapter::Detect::Detect");
+        CV_Assert(detector);
+    }
+
+    void detect(const cv::Mat &Image, std::vector<cv::Rect> &objects)
+    {
+        LOGD("CascadeDetectorAdapter::Detect: begin");
+        LOGD("CascadeDetectorAdapter::Detect: scaleFactor=%.2f, minNeighbours=%d, minObjSize=(%dx%d), maxObjSize=(%dx%d)", scaleFactor, minNeighbours, minObjSize.width, minObjSize.height, maxObjSize.width, maxObjSize.height);
+        Detector->detectMultiScale(Image, objects, scaleFactor, minNeighbours, 0, minObjSize, maxObjSize);
+        LOGD("CascadeDetectorAdapter::Detect: end");
+    }
+
+    virtual ~CascadeDetectorAdapter()
+    {
+        LOGD("CascadeDetectorAdapter::Detect::~Detect");
+    }
+private:
+    CascadeDetectorAdapter();
+    cv::Ptr<cv::CascadeClassifier> Detector;
+};
