8cb756ed061f8c95d9cb426988d48e6cc290c552
Lu Xuan <myfather103@163.com>
2019-07-11 22:07:46 +0800

    提交答题卡识别代码。

--- answersheetscan/src/main/java/com/luxuan/answersheetscan/view/CropActivity.java
+++ answersheetscan/src/main/java/com/luxuan/answersheetscan/view/CropActivity.java
@@ -2,11 +2,13 @@ package com.luxuan.answersheetscan.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.luxuan.answersheetscan.R;
 import com.luxuan.answersheetscan.presenter.CropPresenter;
 import com.luxuan.answersheetscan.presenter.CropPresenterImpl;
+import com.luxuan.answersheetscan.utils.ToastUtils;
 
 import me.pqpo.smartcropperlib.view.CropImageView;
 
@@ -28,5 +30,33 @@ public class CropActivity extends Activity {
 
     private void initData(){
         mPresenter = new CropPresenterImpl(this);
+    }
+
+    private void initView(){
+        mImageViewCrop=findViewById(R.id.iv_crop);
+        mTextViewCancel=findViewById(R.id.tv_exit);
+        mTextViewConfirm=findViewById(R.id.tv_confirm);
+        mTextViewCancel.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+        });
+
+        mTextViewConfirm.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                if(mImageViewCrop.canRightCrop()){
+                    mPresenter.crop(mImageViewCrop);
+                }else{
+                    ToastUtils.showToast(CropActivity.this, "初始化失败");
+                }
+            }
+        });
+
+        mPresenter.analyzeSrcBitmap(getIntent().getStringExtra("filePath"));
     }
 }
