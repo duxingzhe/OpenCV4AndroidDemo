e66baac85672dd760a11a34f4264a5b2d664e6ae
Lu Xuan <myfather103@163.com>
2019-07-15 07:38:11 +0800

    提交颜色斑点检测代码。

--- colorblob/src/main/java/com/luxuan/colorblob/ColorBlobDetectionActivity.java
+++ colorblob/src/main/java/com/luxuan/colorblob/ColorBlobDetectionActivity.java
@@ -2,12 +2,64 @@ package com.luxuan.colorblob;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class ColorBlobDetectionActivity extends Activity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+
+public class ColorBlobDetectionActivity extends Activity implements View.OnTouchListener, CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final String TAG ="OCVSample::Activity";
+
+    private boolean mIsColorSelected=false;
+    private Mat mRgba;
+    private Scalar mBlobColorRgba;
+    private Scalar mBlobColorHsv;
+    private ColorBlobDetector mDetector;
+    private Mat mSpectrum;
+    private Size SPECTRUM_SIZE;
+    private Scalar CONTOUR_COLOR;
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    private BaseLoaderCallback mLoaderCallback=new BaseLoaderCallback(this){
+        @Override
+        public void onManagerConnected(int status){
+            switch(status){
+                case LoaderCallbackInterface.SUCCESS:
+                    Log.i(TAG, "OpenCV loaded successfully.");
+                    mOpenCvCameraView.enableView();
+                    mOpenCvCameraView.setOnTouchListener(ColorBlobDetectionActivity.this);
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+    public ColorBlobDetectionActivity{
+        Log.i(TAG, "Instantiated new "+this.getClass());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         setContentView(R.layout.color_blob_detection_surface_view);
+
+        mOpenCvCameraView=(CameraBridgeViewBase)findViewById(R.id.color_blob_detection_activity_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
     }
 }
