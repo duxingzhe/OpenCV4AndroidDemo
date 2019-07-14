cb3f21d43976f4d83fdc045f1dac4f5661ff815b
Lu Xuan <myfather103@163.com>
2019-07-14 09:36:42 +0800

    提交颜色斑点检测代码。

---
+++ colorblob/src/main/java/com/luxuan/colorblob/ColorBlobDetectionActivity.java
@@ -0,0 +1,13 @@
+package com.luxuan.colorblob;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ColorBlobDetectionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.color_blob_detection_surface_view);
+    }
+}
