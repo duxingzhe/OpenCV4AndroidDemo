7eb68de4133b4f361537b3d8481ee80bc5a4bcbf
Lu Xuan <myfather103@163.com>
2019-07-16 16:27:45 +0800

    提交颜色斑点检测代码。

--- colorblob/src/main/java/com/luxuan/colorblob/ColorBlobDetectionActivity.java
+++ colorblob/src/main/java/com/luxuan/colorblob/ColorBlobDetectionActivity.java
@@ -11,6 +11,8 @@ import android.view.WindowManager;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
@@ -61,5 +63,44 @@ public class ColorBlobDetectionActivity extends Activity implements View.OnTouch
         mOpenCvCameraView=(CameraBridgeViewBase)findViewById(R.id.color_blob_detection_activity_surface_view);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        if(mOpenCvCameraView!=null){
+            mOpenCvCameraView.disableView();
+        }
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization.");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallback);
+        }else{
+            Log.d(TAG, "OpenCV library found inside package. Using it.");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        if(mOpenCvCameraView!=null){
+            mOpenCvCameraView.disableView();
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height){
+        mRgba=new Mat(height, width, CvType.CV_8UC4);
+        mDetector=new ColorBlobDetector();
+        mSpectrum=new Mat();
+        mBlobColorRgba=new Scalar(255);
+        mBlobColorHsv=new Scalar(255);
+        SPECTRUM_SIZE=new Size(200, 64);
+        CONTOUR_COLOR=new Scalar(255,0,0,255);
     }
 }
