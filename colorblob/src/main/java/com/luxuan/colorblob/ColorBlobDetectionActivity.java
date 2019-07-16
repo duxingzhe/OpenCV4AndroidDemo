e38d47812a2c4f14ef0523df7232ad472d68cbc4
Lu Xuan <myfather103@163.com>
2019-07-16 16:37:27 +0800

    提交颜色斑点检测代码。

--- colorblob/src/main/java/com/luxuan/colorblob/ColorBlobDetectionActivity.java
+++ colorblob/src/main/java/com/luxuan/colorblob/ColorBlobDetectionActivity.java
@@ -3,6 +3,7 @@ package com.luxuan.colorblob;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
@@ -14,8 +15,12 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.List;
 
 public class ColorBlobDetectionActivity extends Activity implements View.OnTouchListener, CameraBridgeViewBase.CvCameraViewListener2 {
 
@@ -48,7 +53,7 @@ public class ColorBlobDetectionActivity extends Activity implements View.OnTouch
         }
     };
 
-    public ColorBlobDetectionActivity{
+    public ColorBlobDetectionActivity(){
         Log.i(TAG, "Instantiated new "+this.getClass());
     }
 
@@ -102,5 +107,58 @@ public class ColorBlobDetectionActivity extends Activity implements View.OnTouch
         mBlobColorHsv=new Scalar(255);
         SPECTRUM_SIZE=new Size(200, 64);
         CONTOUR_COLOR=new Scalar(255,0,0,255);
+    }
+
+    @Override
+    public void onCameraViewStopped(){
+        mRgba.release();
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event){
+        int cols=mRgba.cols();
+        int rows=mRgba.rows();
+
+        int xOffset=(mOpenCvCameraView.getWidth()-cols)/2;
+        int yOffset=(mOpenCvCameraView.getHeight()-rows)/2;
+
+        int x=(int)event.getX()-xOffset;
+        int y=(int)event.getY()-yOffset;
+
+        Log.i(TAG, "Touch image coordinates: ("+ x+", "+y+")");
+
+        if((x<0)||(y<0)||(x>cols)||(y>rows)) {
+            return false;
+        }
+
+        return false;
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame){
+        mRgba=inputFrame.rgba();
+
+        if(mIsColorSelected){
+            mDetector.process(mRgba);
+            List<MatOfPoint> contours=mDetector.getContours();
+            Log.e(TAG,"Contours count: "+contours.size());
+            Imgproc.drawContours(mRgba, contours, -1, CONTOUR_COLOR);
+
+            Mat colorLabel=mRgba.submat(4, 68, 4, 68);
+            colorLabel.setTo(mBlobColorRgba);
+
+            Mat spectrumLabel=mRgba.submat(4, 4+mSpectrum.rows(), 70, 70+mSpectrum.cols());
+            mSpectrum.copyTo(spectrumLabel);
+        }
+
+        return mRgba;
+    }
+
+    private Scalar convertScalarHsv2Rgba(Scalar hsvColor){
+        Mat pointMatRgba=new Mat();
+        Mat pointMatHsv=new Mat(1, 1, CvType.CV_8UC3, hsvColor);
+        Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL);
+
+        return new Scalar(pointMatRgba.get(0,0));
     }
 }
