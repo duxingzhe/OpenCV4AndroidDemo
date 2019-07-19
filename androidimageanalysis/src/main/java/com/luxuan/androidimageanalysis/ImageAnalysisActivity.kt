8545298923ed6f6196959c303f5f450c61bc919f
Lu Xuan <myfather103@163.com>
2019-07-20 07:16:08 +0800

    提交图片分析代码。

--- androidimageanalysis/src/main/java/com/luxuan/androidimageanalysis/ImageAnalysisActivity.kt
+++ androidimageanalysis/src/main/java/com/luxuan/androidimageanalysis/ImageAnalysisActivity.kt
@@ -1,6 +1,7 @@
 package com.luxuan.androidimageanalysis
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -9,10 +10,16 @@ import android.os.MessageQueue
 import android.provider.MediaStore
 import android.support.v4.content.FileProvider
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import kotlinx.android.synthetic.main.activity_image_analysis.*
 import java.io.File
+import java.io.IOException
 import java.util.concurrent.Executor
 import java.util.concurrent.ScheduledThreadPoolExecutor
 import java.util.concurrent.ThreadFactory
@@ -115,4 +122,39 @@ class ImageAnalysisActivity: AppCompatActivity(), View.OnClickListener{
         takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, currentTakePhotoUri)
         startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST_CODE)
     }
+
+    private fun handleInputPhoto(imageUri: Uri?){
+        Glide.with(this@ImageAnalysisActivity).asBitmap().listener(object: RequestListener<Bitmap> {
+
+            override fun onLoadFailed(e: GlideException?, model: Any, target: Target<Bitmap>, isFirstResource: Boolean): Boolean{
+                Toast.makeText(this@ImageAnalysisActivity, "图片加载失败", Toast.LENGTH_SHORT).show()
+                return false
+            }
+
+            override fun onResourceReady(resource: Bitmap, model: Any, target: Target<Bitmap>, dataSource: DataSource, isFirstResource: Boolean): Boolean{
+                startImageClassifier(resource)
+                return false
+            }
+        }).load(imageUri).into(ivPicture)
+        tvInfo.text="Processing..."
+    }
+
+    private fun startImageClassifier(bitmap : Bitmap){
+        executor?.execute{
+            try{
+                Log.i("111", Thread.currentThread().name+" startImageClassifier")
+                val croppedBitmap=getScaledBitmap(bitmap, INPUT_SIZE)
+
+                val results=classifier?.recognizeImage(croppedBitmap)
+                Log.i("111", "startImageClassifier results: $results")
+                runOnUiThread{
+                    tvInfo.text=String.format("results: %s", results)
+                }
+            }catch(e: IOException){
+                Log.e("111", "startImageClassifier getScaleBitmap "+ e.message)
+                e.printStackTrace()
+            }
+        }
+    }
+
 }
