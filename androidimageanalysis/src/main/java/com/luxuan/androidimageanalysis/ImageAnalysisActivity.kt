0b10f424fc8660604ecd4feb1ff0d25339f4b0cd
luxuan <myfather103@163.com>
2019-07-19 11:27:28 +0800

    添加图片分析代码。

--- androidimageanalysis/src/main/java/com/luxuan/androidimageanalysis/ImageAnalysisActivity.kt
+++ androidimageanalysis/src/main/java/com/luxuan/androidimageanalysis/ImageAnalysisActivity.kt
@@ -1,6 +1,8 @@
 package com.luxuan.androidimageanalysis
 
 import android.net.Uri
+import android.os.Bundle
+import android.os.Looper
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import java.util.concurrent.Executor
@@ -10,7 +12,7 @@ class ImageAnalysisActivity: AppCompatActivity(), View.OnClickListener{
     private val TAKE_PHOTO_REQUEST_CODE=120
     private val PICTURE_REQUEST_CODE=911
 
-    private val CURRENT_REQUEST_CODE="currentTakePhotoUri"
+    private val CURRENT_TAKE_PHOTO_URI="currentTakePhotoUri"
 
     private val INPUT_SIZE=224
     private val IMAGE_MEAN=117
@@ -23,5 +25,26 @@ class ImageAnalysisActivity: AppCompatActivity(), View.OnClickListener{
     private var executor: Executor?=null
     private var currentTakePhotoUri: Uri?=null
     private var classifer: Classifier?=null
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_analysis)
+
+        ivChoose.setOnClickListener(this)
+        ivTakePhoto.setOnClickListener(this)
+        Looper.myQueue().addIdleHandler(idleHandler)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?){
+        outState?.putParcelable(CURRENT_TAKE_PHOTO_URI, currentTakePhotoUri)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?){
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState!=null){
+            currentTakePhotoUri=savedInstanceState.getParcelable(CURRENT_TAKE_PHOTO_URI)
+        }
+    }
 
 }
