b2d3111b27a7168451b78c4b45cdc32da8774052
Lu Xuan <myfather103@163.com>
2019-08-10 07:17:25 +0800

    提交相机校准代码。

--- cameracalibration/src/main/java/com/luxuan/cameracalibration/CameraCalibrationActivity.java
+++ cameracalibration/src/main/java/com/luxuan/cameracalibration/CameraCalibrationActivity.java
@@ -2,8 +2,44 @@ package com.luxuan.cameracalibration;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class CameraCalibrationActivity extends Activity {
+import com.luxuan.cameracalibration.Render.OnCameraFrameRender;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+
+public class CameraCalibrationActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2, View.OnTouchListener {
+
+    private static final String TAG="OCVSample::Activity";
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private CameraCalibrator mCalibrator;
+    private OnCameraFrameRender mOnCameraFrameRender;
+    private int mWidth;
+    private int mHeight;
+
+    private BaseLoaderCallback mLoaderCallback=new BaseLoaderCallback(this){
+        @Override
+        public void onManagerConnected(int status){
+            switch(status){
+                case LoaderCallbackInterface.SUCCESS:
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                    mOpenCvCameraView.setOnTouchListener(CameraCalibrationActivity.this);
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+    public CameraCalibrationActivity(){
+        Log.i(TAG, "Instantiated new "+this.getClass());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
