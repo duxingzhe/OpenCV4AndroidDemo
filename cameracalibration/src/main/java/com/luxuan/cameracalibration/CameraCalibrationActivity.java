ba844aec2b19069d49426d55416cb8558f6db543
Lu Xuan <myfather103@163.com>
2019-08-11 07:55:24 +0800

    提交相机校准代码。

--- cameracalibration/src/main/java/com/luxuan/cameracalibration/CameraCalibrationActivity.java
+++ cameracalibration/src/main/java/com/luxuan/cameracalibration/CameraCalibrationActivity.java
@@ -3,13 +3,16 @@ package com.luxuan.cameracalibration;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.luxuan.cameracalibration.Render.OnCameraFrameRender;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
 
 public class CameraCalibrationActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2, View.OnTouchListener {
 
@@ -43,7 +46,42 @@ public class CameraCalibrationActivity extends Activity implements CameraBridgeV
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         setContentView(R.layout.camera_calibration_surface_view);
+
+        mOpenCvCameraView=(CameraBridgeViewBase)findViewById(R.id.camera_calibration_java_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        if(mOpenCvCameraView!=null){
+            mOpenCvCameraView.disableView();
+        }
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallback);
+        }else{
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        if(mOpenCvCameraView!=null){
+            mOpenCvCameraView.disableView();
+        }
     }
 }
