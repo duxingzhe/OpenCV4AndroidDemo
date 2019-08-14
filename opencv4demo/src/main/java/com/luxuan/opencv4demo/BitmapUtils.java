e200bcc6f754aa2ecd090bf81b05870b15bc67e0
Lu Xuan <myfather103@163.com>
2019-08-15 07:10:25 +0800

    提交OpenCV4示例代码。

--- opencv4demo/src/main/java/com/luxuan/opencv4demo/BitmapUtils.java
+++ opencv4demo/src/main/java/com/luxuan/opencv4demo/BitmapUtils.java
@@ -10,68 +10,31 @@ import java.io.FileOutputStream;
 
 public class BitmapUtils {
 
-    private BitmapUtils(){
-
-    }
-
-    public static File bitmapToFile(Bitmap bitmap, String savePath, String saveName){
-        File dirFile=new File(savePath);
-        if(!dirFile.exists()){
-            dirFile.mkdirs();
-        }
-        File targetFile=new File(savePath+saveName+".jpg");
-        BufferedOutputStream bos=null;
-        try{
-            bos=new BufferedOutputStream(new FileOutputStream(targetFile));
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 80, bos);
-            bos.flush();
-            bos.close();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-
-        return targetFile;
-    }
-
-    public static Bitmap resizeImage(Bitmap bitmap, int maxWidth, int maxHeight){
+    public static Bitmap changeChannels(Bitmap bitmap){
         int width=bitmap.getWidth();
         int height=bitmap.getHeight();
-        Float newWidth;
-        Float newHeight;
-        if(width>maxWidth){
-            newHeight=1.0F*maxWidth/width*height;
-            newWidth=1.0F*maxWidth;
-            if(newHeight>maxHeight){
-                newWidth=maxHeight/newHeight*newWidth;
-                newHeight=1.0F*maxHeight;
+        int[] pixels=new int[width*height];
+        bitmap.getPixels(pixels, 0, width, 0, 0, width,height);
+
+        int index=0;
+        int channel1;
+        int channel2;
+        int channel3;
+        int channel4;
+
+        for(int row=0;row<height;row++){
+            for(int col=0;col<width;col++){
+                int pixel=pixels[index];
+                channel1=(pixel>>24)&0xff;
+                channel2=(pixel>>16)&0xff;
+                channel3=(pixel>>8)&0xff;
+                channel4=pixel&0xff;
+                pixel=((channel1&0xff)<<24|(channel4&0xff)<<16|(channel3&0xff)<<8|(channel2&0xff));
+                pixels[index]=pixel;
+                index++;
             }
-        }else if(height>maxHeight){
-            newWidth=1.0F*maxHeight/height*width;
-            newHeight=1.0f*maxHeight;
-            if(newWidth>maxWidth){
-                newHeight=maxWidth/newWidth*newHeight;
-                newWidth=1.0F*maxWidth;
-            }
-        }else{
-            newWidth=1.0F*maxWidth;
-            newHeight=1.0F*maxHeight;
         }
-        float scaleWidth=newWidth/width;
-        float scaleHeight=newHeight/height;
-        Matrix matrix=new Matrix();
-        matrix.postScale(scaleWidth, scaleHeight);
-        return Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, true);
-    }
-
-    private static float getDensity(Context context){
-        return context.getApplicationContext().getResources().getDisplayMetrics().density;
-    }
-
-    public static int dpToPx(Context context, Float dpValue){
-        return (int)(dpValue*getDensity(context)+0.5F);
-    }
-
-    public static int pxToDp(Context context, Float pxValue){
-        return (int)(pxValue/getDensity(context)+0.5F);
+        bitmap.setPixels(pixels, 0, width, 0, 0, width,height);
+        return bitmap;
     }
 }
