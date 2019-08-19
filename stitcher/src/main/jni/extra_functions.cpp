9674789b79cf9bfc0683caddb77d1c3971892b8b
Lu Xuan <myfather103@163.com>
2019-08-19 08:51:28 +0800

    添加图片拼接代码。

--- stitcher/src/main/jni/extra_functions.cpp
+++ stitcher/src/main/jni/extra_functions.cpp
@@ -74,3 +74,80 @@ Mat get_modified_lighten(Mat img)
     cv::addWeighted(brightened_image, 0.9, img, 0.1, 3, brightened_image);
     return brightened_image;
 }
+
+jobject mat_to_bitmapp(JNIEnv *env, Mat &src, bool needPremultiplyAlpha, jobject bitmap_config)
+{
+    jclass java_bitmap_class=(jclass)env->FindClass("android/graphics/Bitmap");
+    jmethodID mid=env->GetStaticMethodID(java_bitmap_class, "createBitmap",
+                                         "(IILandroid/graphics/Bitmap$Config;)Landroid/graphics/Bitmap;");
+
+    jobject bitmap=env->CallStaticObjectMethod(java_bitmap_class, mid, src.size().width, src.size().height, bitmap_config);
+
+    AndroidBitmapInfo info;
+    void* pixels=0;
+
+    try
+    {
+        CV_Assert(AndroidBitmap_getInfo(env, bitmap, &info)>=0);
+        CV_Assert(src.type()==CV_8UC1||src.type()==CV_8UC3||src.type()==CV_8UC4);
+        CV_Assert(AndroidBitmap_lockPixels(env,bitmap, &pixels)>=0);
+        CV_Assert(pixels);
+        if(info.format==ANDORID_BITMAP_FORMAT_RGBA_8888)
+        {
+            Mat tmp(info.height, info.width, CV_8UC4, pixels);
+            if(src.type()==CV_8UC1)
+            {
+                cvtColor(src, tmp, CV_GRAY2RGBA);
+            }
+            else if(src.type()==CV_8UC3)
+            {
+                cvtColor(src, tmp, CV_BGR2RGBA);
+            }
+            else if(src.type()==CV_8UC4)
+            {
+                if(needPremultiplyAlpha)
+                {
+                    cvtColor(src, tmp, CV_RGBA2mRGBA);
+                }
+                else
+                {
+                    src.copyTo(tmp);
+                }
+            }
+        }
+        else
+        {
+            Mat tmp(info.height, info.width, CV_8UC2, pixels);
+            if(src.type()==CV_8UC1)
+            {
+                cvtColor(src, tmp, CV_GRAY2BGR565);
+            }
+            else if(src.type()==CV_8UC3)
+            {
+                cvtColor(src, tmp, CV_BGR2BGR565);
+            }
+            else if(src.type()==CV_8UC4)
+            {
+                cvtColor(src, tmp, CV_RGBA2BGR565);
+            }
+        }
+        AndroidBitmap_unlockPixels(env, bitmap);
+        return bitmap;
+    }
+    catch(cv::Exception e)
+    {
+        AndroidBitmap_unlockPixels(env, bitmap);
+        jclass je=env->FindClass("org/opencv/core/CvException");
+        if(!je)
+            je=env->FindClass("java/lang/Exception");
+        env->ThrowNew(je, e.what());
+        return bitmap;
+    }
+    catch(...)
+    {
+        AndroidBitmap_unlockPixels(env, bitmap);
+        jclass je=env->FindClass("java/lang/Exception");
+        env->ThrowNew(je, "Unknown exception in JNI code {nMatToBitmap}");
+        return bitmap;
+    }
+}
