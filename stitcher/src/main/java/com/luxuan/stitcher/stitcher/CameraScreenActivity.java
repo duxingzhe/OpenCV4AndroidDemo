964038a6996fcf989f5e4633031e43891f7d74ec
Lu Xuan <myfather103@163.com>
2019-08-25 09:33:34 +0800

    添加图片拼接代码。

--- stitcher/src/main/java/com/luxuan/stitcher/stitcher/CameraScreenActivity.java
+++ stitcher/src/main/java/com/luxuan/stitcher/stitcher/CameraScreenActivity.java
@@ -3,6 +3,7 @@ package com.luxuan.stitcher.stitcher;
 import android.content.Context;
 import android.hardware.Camera;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.luxuan.stitcher.R;
 
 public class CameraScreenActivity extends AppCompatActivity {
 
@@ -45,4 +48,40 @@ public class CameraScreenActivity extends AppCompatActivity {
     private Context mContext;
     private LinearLayout picturecount;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_camera_screen);
+        mContext=this;
+
+        extra_settings=(LinearLayout)findViewById(R.id.extra_settings);
+        camera_preview=(LinearLayout)findViewById(R.id.camera_preview);
+
+        flashOn=(LinearLayout)findViewById(R.id.flashon);
+        flashOff=(LinearLayout)findViewById(R.id.flashoff);
+
+        setting_before=(LinearLayout)findViewById(R.id.setting_bef);
+        setting_after=(LinearLayout)findViewById(R.id.setting_aft);
+        camera_single=(LinearLayout)findViewById(R.id.camera_single);
+        camera_batch=(LinearLayout)findViewById(R.id.camera_batch);
+        batch_before=(LinearLayout)findViewById(R.id.batch_bef);
+        batch_after=(LinearLayout)findViewById(R.id.batch_aft);
+        single_before=(LinearLayout)findViewById(R.id.single_bef);
+        single_after=(LinearLayout)findViewById(R.id.single_afr);
+
+        flash_off=(ImageView)findViewById(R.id.flash_off);
+        flash_on=(ImageView)findViewById(R.id.flash_on);
+
+        modeTextView=(TextView)findViewById(R.id.textView);
+        picturecount=(LinearLayout)findViewById(R.id.picturecount);
+
+        pictureImageView=(ImageView)findViewById(R.id.pictureIV);
+        pictureCountBackgroundView=(View)findViewById(R.id.pictureCountBgV);
+        pictureCountBackgroundTextView=(TextView)findViewById(R.id.pictureCountTV);
+
+        settingTest=(LinearLayout)findViewById(R.id.settingTest);
+        settingtest=(ImageView)findViewById(R.id.settingtest);
+
+        initialize();
+    }
 }
