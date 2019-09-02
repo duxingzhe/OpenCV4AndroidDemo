485652a67eb349c78f7db9ed73c1644cdbe78d9c
Lu Xuan <myfather103@163.com>
2019-09-02 19:14:43 +0800

    添加图片拼接代码。

--- stitcher/src/main/java/com/luxuan/stitcher/stitcher/widget/PolygonView.java
+++ stitcher/src/main/java/com/luxuan/stitcher/stitcher/widget/PolygonView.java
@@ -2,9 +2,19 @@ package com.luxuan.stitcher.stitcher.widget;
 
 import android.content.Context;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+
+import com.luxuan.stitcher.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PolygonView extends FrameLayout {
 
@@ -65,5 +75,54 @@ public class PolygonView extends FrameLayout {
         addView(pointer4);
 
         initPaint();
+    }
+
+    @Override
+    public void attachViewToParent(View child, int index, ViewGroup.LayoutParams params){
+        super.attachViewToParent(child, index, params);
+    }
+
+    private void initPaint(){
+        paint=new Paint();
+        paint.setColor(getResources().getColor(R.color.blue));
+        paint.setStrokeWidth(2);
+        paint.setAntiAlias(true);
+    }
+
+    public Map<Integer, PointF> getPoints(){
+        List<PointF> points=new ArrayList<>();
+        points.add(new PointF(pointer1.getX(), pointer1.getY()));
+        points.add(new PointF(pointer2.getX(), pointer2.getY()));
+        points.add(new PointF(pointer3.getX(), pointer3.getY()));
+        points.add(new PointF(pointer4.getX(), pointer4.getY()));
+
+        return getOrderedPoints(points);
+    }
+
+    public Map<Integer, PointF> getOrderedPoints(List<PointF> points){
+        PointF centerPoint=new PointF();
+        int size=points.size();
+        for(PointF pointF: points){
+            centerPoint.x+=pointF.x/size;
+            centerPoint.y+=pointF.y/size;
+        }
+
+        Map<Integer, PointF> orderedPoints=new HashMap<>();
+        for(PointF pointF: points){
+            int index=-1;
+            if(pointF.x<centerPoint.x&&pointF.y<centerPoint.y){
+                index=0;
+            }else if(pointF.x>centerPoint.x&&pointF.y<centerPoint.y){
+                index=1;
+            }else if(pointF.x<centerPoint.x&&pointF.y>centerPoint.y){
+                index=2;
+            }else if(pointF.x>centerPoint.x&&pointF.y>centerPoint.y){
+                index=3;
+            }
+
+            orderedPoints.put(index, pointF);
+        }
+
+        return orderedPoints;
     }
 }
