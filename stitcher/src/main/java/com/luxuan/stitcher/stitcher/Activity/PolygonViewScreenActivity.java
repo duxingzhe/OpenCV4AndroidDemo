ae0b4f71e324fd2ec0cc8b158df43d6f8cd2a876
Lu Xuan <myfather103@163.com>
2019-09-08 08:03:48 +0800

    添加图片拼接代码。

--- stitcher/src/main/java/com/luxuan/stitcher/stitcher/Activity/PolygonViewScreenActivity.java
+++ stitcher/src/main/java/com/luxuan/stitcher/stitcher/Activity/PolygonViewScreenActivity.java
@@ -26,6 +26,9 @@ import com.luxuan.stitcher.stitcher.widget.PolygonView;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static java.lang.Math.abs;
@@ -275,5 +278,50 @@ public class PolygonViewScreenActivity extends AppCompatActivity {
         result.setPixels(resultPixels, 0, width, 0, 0, width, height);
         polygonView.setVisibility(View.GONE);
         return result;
+    }
+
+    private Map<Integer, PointF> getEdgePoints(Bitmap tempBitmap){
+        List<PointF> pointFs=getContourEdgePoints(tempBitmap);
+        Map<Integer, PointF> orderedPoints=orderedValidEdgePoints(tempBitmap, pointFs);
+        return orderedPoints;
+    }
+
+    private List<PointF> getContourEdgePoints(Bitmap tempBitmap){
+        int w=tempBitmap.getWidth(), h=tempBitmap.getHeight();
+        int[] pixels=new int[w*h];
+        tempBitmap.getPixels(pixels, 0, w, 0, 0, w, h);
+        int[] points=OpenCVHelper.getBoxPoints(pixels, w, h);
+        float x1=points[0];
+        float y1=points[1];
+        float x2=points[2];
+        float y2=points[3];
+        float x3=points[4];
+        float y3=points[5];
+        float x4=points[6];
+        float y4=points[7];
+
+        List<PointF> pointFs=new ArrayList<>();
+        pointFs.add(new PointF(x1,y1));
+        pointFs.add(new PointF(x2,y2));
+        pointFs.add(new PointF(x3,y3));
+        pointFs.add(new PointF(x4,y4));
+        return pointFs;
+    }
+
+    private Map<Integer, PointF> getOutlinePoints(Bitmap tempBitmap){
+        Map<Integer,PointF> outlinePoints=new HashMap<>();
+        outlinePoints.put(0, new PointF(0,0));
+        outlinePoints.put(1, new PointF(tempBitmap.getWidth(),0));
+        outlinePoints.put(2, new PointF(0,tempBitmap.getHeight()));
+        outlinePoints.put(3, new PointF(tempBitmap.getWidth(),tempBitmap.getHeight()));
+        return outlinePoints;
+    }
+
+    private Map<Integer, PointF> orderedValidEdgePoints(Bitmap tempBitmap, List<PointF> pointFs){
+        Map<Integer, PointF> orderedPoints=polygonView.getOrderedPoints(pointFs);
+        if(!polygonView.isValidShape(orderedPoints)){
+            orderedPoints=getOutlinePoints(tempBitmap);
+        }
+        return orderedPoints;
     }
 }
