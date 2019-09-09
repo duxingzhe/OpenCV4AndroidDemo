09a2f284e7b976ecd0b69d1759f344539d7d0e15
Lu Xuan <myfather103@163.com>
2019-09-09 08:33:17 +0800

    添加图片拼接代码。

--- stitcher/src/main/java/com/luxuan/stitcher/stitcher/Activity/ResultActivity.java
+++ stitcher/src/main/java/com/luxuan/stitcher/stitcher/Activity/ResultActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.luxuan.stitcher.R;
+import com.luxuan.stitcher.stitcher.Util.OpenCVHelper;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -26,6 +28,37 @@ public class ResultActivity extends AppCompatActivity {
         Bitmap bitmap=(Bitmap)intent.getParcelableExtra("BitmapImage");
         Toast.makeText(ResultActivity.this, bitmap.toString(), Toast.LENGTH_SHORT).show();
 
-        black_white.setOnClickListener(new BWButtonClikcListener());
+        black_white.setOnClickListener(new BWButtonClickListener());
+    }
+
+    private Bitmap JPGtoRGB8888(Bitmap img){
+        Bitmap result=null;
+
+        int numPixels=img.getWidth()*img.getHeight();
+        int[] pixels=new int[numPixels];
+
+        img.getPixels(pixels, 0, img.getWidth(), 0, 0, img.getWidth(), img.getHeight());
+
+        result=Bitmap.createBitmap(img.getWidth(), img.getHeight(), Bitmap.Config.ARGB_8888);
+
+        result.getPixels(pixels, 0, result.getWidth(), 0, 0, result.getWidth(), result.getHeight());
+
+        return result;
+    }
+    private class BWButtonClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view){
+            colorChange();
+        }
+    }
+
+    private void colorChange(){
+        op= OpenCVHelper.getMagicColorBitmap(original);
+        image.setImageBitmap(op);
+    }
+
+    static{
+        System.loadLibrary("OpenCV");
     }
 }
