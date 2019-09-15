bf86b938fa0e52065f1b14536ef8677e728677e3
Lu Xuan <myfather103@163.com>
2019-09-15 08:16:00 +0800

    添加图片拼接代码。

--- stitcher/src/main/java/com/luxuan/stitcher/stitcher/Activity/MainActivity.java
+++ stitcher/src/main/java/com/luxuan/stitcher/stitcher/Activity/MainActivity.java
@@ -1,11 +1,30 @@
 package com.luxuan.stitcher.stitcher.Activity;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.luxuan.stitcher.R;
+import com.luxuan.stitcher.stitcher.widget.PolygonView;
+
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView iv_show_img;
+    private PolygonView polygonView;
+    private String imgDecodeStr;
+    private Button scanButton;
+    private FrameLayout sourceFrameLayout;
+    private Uri imageUri;
+    private Bitmap bitmap, op, op2;
+    private int status=0;
+    private FileOutputStream fos;
+    private float perWidth, perHieght;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
