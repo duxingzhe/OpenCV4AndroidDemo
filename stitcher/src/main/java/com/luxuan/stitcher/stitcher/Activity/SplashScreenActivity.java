04f3165f7bf8a074b23aabc9b73a154482b4abb4
Lu Xuan <myfather103@163.com>
2019-09-15 08:12:13 +0800

    添加图片拼接代码。

--- stitcher/src/main/java/com/luxuan/stitcher/stitcher/Activity/SplashScreenActivity.java
+++ stitcher/src/main/java/com/luxuan/stitcher/stitcher/Activity/SplashScreenActivity.java
@@ -1,7 +1,38 @@
 package com.luxuan.stitcher.stitcher.Activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
+import com.luxuan.stitcher.R;
+
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static boolean splashLoaded=false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        if(!splashLoaded){
+            int secondsDelayed=1;
+            new Handler().postDelayed(new Runnable(){
+
+                @Override
+                public void run(){
+                    startActivity(new Intent(SplashScreenActivity.this, HomeActivity.class));
+                    finish();
+                }
+            }, secondsDelayed*1500);
+            splashLoaded=true;
+        }else{
+            Intent intent=new Intent(SplashScreenActivity.this, HomeActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(intent);
+            finish();
+        }
+    }
 
 }
